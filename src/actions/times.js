71d2552b3e695ae8dd58cbcfbf6d56dbd7f6d403
Michael Henry <michaeljhenry@hotmail.ca>
2020-03-18 23:47:10 -0400

    first to heroku..

--- src/actions/times.js
+++ src/actions/times.js
@@ -1,8 +1,11 @@
 import uuid from 'uuid';
+import database from '../firebase/firebase';
+
 
 export const addTime = ({dispenseDay, hourOne, minuteOne, secondOne, hourTwo, minuteTwo, secondTwo}) => {
     const id = uuid();
     const time = {id, dispenseDay,hourOne, minuteOne, secondOne, hourTwo, minuteTwo, secondTwo }
+    database.ref().push(time)
     return{
         type: 'ADD_TIME',
         time
